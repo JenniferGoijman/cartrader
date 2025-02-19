3b9c2452d79510f72f42743a7bd5605ba48e2877
Jennifer Goijman <jennifer@journi.uk>
2025-02-19 13:32:10 +0100

    feat: add Search Bar

--- app.vue
+++ app.vue
@@ -9,6 +9,10 @@
       <div class="z-10 absolute w-full h-full flex justify-center items-center">
         <div class="text-center w-1/2">
           <h1 class="text-8xl font-serif mb-10 text-white">Find your drive</h1>
+          <div class="font-serif text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto">
+            <input type="text" class="py-3 px-5 w-full text-2xl rounded-full focus: outline-none" placeholder="Search by city..." />
+            <button class="bg-green-500 px-10 text-white">Search</button>
+          </div>
         </div>
       </div>
 
