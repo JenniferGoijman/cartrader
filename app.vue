5cf13f0491393ed37f9dfc07cc23027ff7a879fc
Jennifer Goijman <jennifer@journi.uk>
2025-02-19 13:27:09 +0100

    feat: add home

--- app.vue
+++ app.vue
@@ -3,5 +3,15 @@
     <header class="sticky top-0 z-50 flex justify-between items center space-x-1 border-b bg-white p-4 shadow-md">
       <a href="" class="text-3xl font-mono">Car Trader</a>
     </header>
+
+    <div class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url(https://www.hautetime.com/wp-content/uploads/2019/01/Lamborghini-SJV1.jpg)]">
+      <div class="z-10 absolute w-full h-full flex justify-center items-center">
+        <div class="text-center w-1/2">
+          <h1 class="text-8xl font-serif mb-10 text-white">Find your drive</h1>
+        </div>
+      </div>
+
+      <div class="z-1 absolute w-full h-full bg-black opacity-50"> </div>
+    </div>
   </div>
 </template>
