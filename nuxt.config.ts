6bfe6eb44370b9a3190562af11f1beab7127e022
Jennifer Goijman <jennifer@journi.uk>
2025-02-19 13:37:19 +0100

    chore: add eslint & prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-02-19",
   devtools: { enabled: true },
-  modules: [
-    "@nuxtjs/tailwindcss"
-  ],
-})
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint"],
+});
