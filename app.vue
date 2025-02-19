dc34ac26dab04544b36e0bd00137484f793f2053
Jennifer Goijman <jennifer@journi.uk>
2025-02-19 13:27:47 +0100

    style: add opacity background

--- app.vue
+++ app.vue
@@ -5,13 +5,13 @@
     </header>
 
     <div class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url(https://www.hautetime.com/wp-content/uploads/2019/01/Lamborghini-SJV1.jpg)]">
+      <div class="z-1 absolute w-full h-full bg-black opacity-50"> </div>
       <div class="z-10 absolute w-full h-full flex justify-center items-center">
         <div class="text-center w-1/2">
           <h1 class="text-8xl font-serif mb-10 text-white">Find your drive</h1>
         </div>
       </div>
 
-      <div class="z-1 absolute w-full h-full bg-black opacity-50"> </div>
     </div>
   </div>
 </template>
