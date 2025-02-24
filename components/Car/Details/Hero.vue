0ebeba89d19f075420684bac730589fc94d839fd
Jennifer Goijman <jennifer@journi.uk>
2025-02-24 13:35:19 +0100

    refactor: split Cars Details page in small components

---
+++ components/Car/Details/Hero.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="mt-10">
+    <img
+      src="https://images.toyota-europe.com/es/product-token/8807ce5b-43c8-440b-9465-567c3ae534c3/vehicle/ff47bc5b-d4be-4f9d-9a49-aa5f0de6fe1f/width/2048/height/1165/scale-mode/1/padding/0/background-colour/FFFFFF/image-quality/75/day-exterior-3_218_LB40.webp"
+      class="w-full"
+      alt=""
+    >
+    <h1 class="mt-10 text-4xl">Toyota Land Cruiser</h1>
+    <div class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between">
+      <div class="flex">
+        <p class="mr-2">5 seats</p>
+        <p class="mr-2">|</p>
+        <p class="mr-2">67,444 miles</p>
+      </div>
+      <div>
+        <p class="font-bold text-2xl">$39,555</p>
+      </div>
+    </div>
+  </div>
+</template>
