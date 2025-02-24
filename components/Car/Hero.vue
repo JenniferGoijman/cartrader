c043c95d0500691c2a6dd61c8f3ed415b8d273ee
Jennifer Goijman <jennifer@journi.uk>
2025-02-24 13:07:32 +0100

    refactor: split Homepage in small components

---
+++ components/Car/Hero.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url(https://www.hautetime.com/wp-content/uploads/2019/01/Lamborghini-SJV1.jpg)]">
+    <div class="z-1 absolute w-full h-full bg-black opacity-50" />
+    <div class="z-10 absolute w-full h-full flex justify-center items-center">
+      <div class="text-center w-1/2">
+        <h1 class="text-8xl font-serif mb-10 text-white">Find your drive</h1>
+        <CarSearchBar />
+    </div>
+  </div>
+</div></template>
