ea45034d4a5883baa555e6a4a4b3071aa9a46f13
Jennifer Goijman <jennifer@journi.uk>
2025-02-24 13:26:22 +0100

    refactor: split Cars page in small components

---
+++ components/Car/Card.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
+    <div class="flex h-full">
+      <img
+        src="https://images.toyota-europe.com/es/product-token/8807ce5b-43c8-440b-9465-567c3ae534c3/vehicle/ff47bc5b-d4be-4f9d-9a49-aa5f0de6fe1f/width/2048/height/1165/scale-mode/1/padding/0/background-colour/FFFFFF/image-quality/75/day-exterior-3_218_LB40.webp"
+        alt="Toyota Land Cruiser"
+        class="w-[300px] h-full"
+      >
+      <div class="p-4 flex flex-col">
+        <h1 class="text-2xl text-green-700">Toyota Land Cruiser 250 VX-L</h1>
+        <p class="text-gray-700">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+      </div>
+      <h1 class="mt-auto text-xl">$39,555</h1>
+    </div>
+  </div>
+</template>
---
+++ components/Car/SideBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="shadow border w-64 mr-10 z-30 h-[190px]">
+    <div class="p-5 flex justify-between relative cursor-pointer border-b">
+      <h3>Location</h3>
+      <h3 class="text-green-500 capitalize">Toronto</h3>
+      <!-- <div class="absolute border shadow left-72 p-5 top-1 -m-1 bg-white">
+        <input type="text" class="border p-1 rounder" >
+        <button class="bg-green-500 w-full mt-2 rounder text-white p-1">Apply</button>
+      </div> -->
+    </div>
+    <div class="p-5 flex justify-between relative cursor-pointer border-b">
+      <h3>Make</h3>
+      <h3 class="text-green-500 capitalize">Any</h3>
+    </div>
+    <div class="p-5 flex justify-between relative cursor-pointer border-b">
+      <h3>Price</h3>
+      <h3 class="text-green-500 capitalize">Any</h3>
+    </div>
+  </div>
+</template>
