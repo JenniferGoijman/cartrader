473fbab4575e230ae0c30c4f75878003887b6cae
Jennifer Goijman <jennifer@journi.uk>
2025-02-24 14:13:37 +0100

    feat: create custom layout

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -8,10 +8,14 @@ function toTitleCase(text) {
 useHead({
   title: toTitleCase(route.params.name)
 })
+
+definePageMeta({
+    layout: 'custom'
+})
 </script>
 
 <template>
-  <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
+  <div>
     <CarDetailsHero />
     <CarDetailsAttributes />
     <CarDetailsDescription />
--- pages/city/[city]/car.vue
+++ pages/city/[city]/car.vue
@@ -8,14 +8,16 @@ function toTitleCase(text) {
 useHead({
   title: `${route.params.brand ? toTitleCase(route.params.brand) : 'Cars'} in ${toTitleCase(route.params.city)}`
 })
+
+definePageMeta({
+    layout: 'custom'
+})
 </script>
 
 <template>
-  <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16">
-    <div class="mt-32 flex">
-      <CarSideBar />
+  <div class="mt-32 flex">
+    <CarSideBar />
 
-      <NuxtPage />
-    </div>
+    <NuxtPage />
   </div>
 </template>
