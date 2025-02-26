904d6e552e75ba2774d7102f3633528d3c269e48
JenniferGoijman <jgoijman@gmail.com>
2025-02-26 13:39:47 +0100

    feat: integrate cars data in Cards component

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -1,16 +1,23 @@
+<script setup>
+const _props = defineProps({
+  // eslint-disable-next-line vue/require-default-prop
+  car: Object,
+})
+</script>
+
 <template>
-  <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
+  <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]" @click="navigateTo(`/car/${car.name}-${car.id}`)">
     <div class="flex h-full">
       <img
-        src="https://images.toyota-europe.com/es/product-token/8807ce5b-43c8-440b-9465-567c3ae534c3/vehicle/ff47bc5b-d4be-4f9d-9a49-aa5f0de6fe1f/width/2048/height/1165/scale-mode/1/padding/0/background-colour/FFFFFF/image-quality/75/day-exterior-3_218_LB40.webp"
-        alt="Toyota Land Cruiser"
+        :src="car.url"
+        :alt="car.name"
         class="w-[300px] h-full"
       >
       <div class="p-4 flex flex-col">
-        <h1 class="text-2xl text-green-700">Toyota Land Cruiser 250 VX-L</h1>
-        <p class="text-gray-700">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+        <h1 class="text-2xl text-green-700">{{ car.name }}</h1>
+        <p class="text-gray-700">{{ car.description }}</p>
       </div>
-      <h1 class="mt-auto text-xl">$39,555</h1>
+      <h1 class="mt-auto text-xl">{{ car.price }}</h1>
     </div>
   </div>
 </template>
