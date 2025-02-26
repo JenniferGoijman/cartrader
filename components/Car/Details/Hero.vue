c76955de0d9b8f2f6396d4228496d0dd900451e1
JenniferGoijman <jgoijman@gmail.com>
2025-02-26 13:40:44 +0100

    feat: integrate cars data in Car Details page

--- components/Car/Details/Hero.vue
+++ components/Car/Details/Hero.vue
@@ -1,19 +1,26 @@
+<script setup>
+const _props = defineProps({
+  // eslint-disable-next-line vue/require-default-prop
+  car: Object
+});
+</script>
+
 <template>
   <div class="mt-10">
     <img
-      src="https://images.toyota-europe.com/es/product-token/8807ce5b-43c8-440b-9465-567c3ae534c3/vehicle/ff47bc5b-d4be-4f9d-9a49-aa5f0de6fe1f/width/2048/height/1165/scale-mode/1/padding/0/background-colour/FFFFFF/image-quality/75/day-exterior-3_218_LB40.webp"
+      :src="car.url"
+      :alt="car.name"
       class="w-full"
-      alt=""
     >
-    <h1 class="mt-10 text-4xl">Toyota Land Cruiser</h1>
+    <h1 class="mt-10 text-4xl">{{ car.name }}</h1>
     <div class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between">
       <div class="flex">
-        <p class="mr-2">5 seats</p>
+        <p class="mr-2">{{ car.seats }} seats</p>
         <p class="mr-2">|</p>
-        <p class="mr-2">67,444 miles</p>
+        <p class="mr-2">{{ car.miles }} miles</p>
       </div>
       <div>
-        <p class="font-bold text-2xl">$39,555</p>
+        <p class="font-bold text-2xl">${{ car.price }}</p>
       </div>
     </div>
   </div>
--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,21 +1,26 @@
 <script setup>
 const route = useRoute();
+const { cars } = useCars();
 const { toTitleCase } = useUtilities();
 
 useHead({
   title: toTitleCase(route.params.name)
 })
 
+const car = computed(() => {
+  return cars.find(({ id }) => id === parseInt(route.params.id))
+});
+
 definePageMeta({
-    layout: 'custom'
+  layout: 'custom'
 })
 </script>
 
 <template>
-  <div>
-    <CarDetailsHero />
-    <CarDetailsAttributes />
-    <CarDetailsDescription />
+  <div v-if="car">
+    <CarDetailsHero :car="car" />
+    <CarDetailsAttributes :features="car.features" />
+    <CarDetailsDescription :description="car.description" />
     <CarDetailsContact />
   </div>
 </template>
