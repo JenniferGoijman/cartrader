0adbb06da9c6b7e6a9be373b72ff7fa5edfac0d5
JenniferGoijman <jgoijman@gmail.com>
2025-02-27 17:34:13 +0100

    refactor: split FilterPrice

--- components/Car/Filters.vue
+++ components/Car/Filters.vue
@@ -5,40 +5,8 @@ const modal = ref({
   price: false,
 });
 
-const priceRange = ref({
-  min: '',
-  max: ''
-});
-const route = useRoute();
-const router = useRouter();
-
-const priceRangeText = computed(() => {
-  const minPrice = route.query.minPrice;
-  const maxPrice = route.query.maxPrice;
-
-  if (!minPrice && !maxPrice) return 'Any';
-  if (!minPrice && maxPrice) return `< $${maxPrice}`;
-  if (minPrice && !maxPrice) return `> $${minPrice}`;
-  return `$${minPrice}-$${route.query.maxPrice}`;
-});
-
 const updateModal = (key) => {
   modal.value[key] = !modal.value[key]
-}
-const onChangePrice = () => {
-  updateModal("price");
-
-  const minPrice = priceRange.value.min;
-  const maxPrice = priceRange.value.max;
-
-  if (minPrice && maxPrice && minPrice > maxPrice) return;
-
-  router.push({
-    query: {
-      minPrice: minPrice,
-      maxPrice: maxPrice,
-    }
-  })
 }
 </script>
 
@@ -46,15 +14,6 @@ const onChangePrice = () => {
   <div class="shadow border w-64 mr-10 z-30 h-[190px]">
     <CarFiltersLocation :open="modal.location" @update-modal="updateModal('location')" />
     <CarFiltersBrand :open="modal.brand" @update-modal="updateModal('brand')" />
-
-    <div class="p-5 flex justify-between relative cursor-pointer border-b">
-      <h3>Price</h3>
-      <h3 class="text-green-500 capitalize" @click="updateModal('price')">{{ priceRangeText }}</h3>
-      <div v-if="modal.price" class="absolute border shadow left-72 p-5 top-1 -m-1 bg-white">
-        <input v-model="priceRange.min" type="number" class="border p-1 rounder" placeholder="Min">
-        <input v-model="priceRange.max" type="number" class="border p-1 rounder" placeholder="Max">
-        <button class="bg-green-500 w-full mt-2 rounder text-white p-1" @click="onChangePrice">Apply</button>
-      </div>
-    </div>
+    <CarFiltersPrice :open="modal.price" @update-modal="updateModal('price')" />
   </div>
 </template>
---
+++ components/Car/FiltersPrice.vue
@@ -0,0 +1,53 @@
+<script setup>
+const _props = defineProps({
+  open: Boolean,
+});
+
+const emit = defineEmits(['update-modal']);
+
+const priceRange = ref({
+  min: '',
+  max: ''
+});
+const route = useRoute();
+const router = useRouter();
+
+
+const priceRangeText = computed(() => {
+  const minPrice = route.query.minPrice;
+  const maxPrice = route.query.maxPrice;
+
+  if (!minPrice && !maxPrice) return 'Any';
+  if (!minPrice && maxPrice) return `< $${maxPrice}`;
+  if (minPrice && !maxPrice) return `> $${minPrice}`;
+  return `$${minPrice}-$${route.query.maxPrice}`;
+});
+
+const onChangePrice = () => {
+  emit('update-modal')
+
+  const minPrice = priceRange.value.min;
+  const maxPrice = priceRange.value.max;
+
+  if (minPrice && maxPrice && minPrice > maxPrice) return;
+
+  router.push({
+    query: {
+      minPrice: minPrice,
+      maxPrice: maxPrice,
+    }
+  })
+}
+</script>
+
+<template>
+  <div class="p-5 flex justify-between relative cursor-pointer border-b">
+    <h3>Price</h3>
+    <h3 class="text-green-500 capitalize" @click="emit('update-modal')">{{ priceRangeText }}</h3>
+    <div v-if="open" class="absolute border shadow left-72 p-5 top-1 -m-1 bg-white">
+      <input v-model="priceRange.min" type="number" class="border p-1 rounder" placeholder="Min">
+      <input v-model="priceRange.max" type="number" class="border p-1 rounder" placeholder="Max">
+      <button class="bg-green-500 w-full mt-2 rounder text-white p-1" @click="onChangePrice">Apply</button>
+    </div>
+  </div>
+</template>
